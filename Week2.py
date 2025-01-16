8edff9d67b74a7975060b79569b4789a257f607f
kebohning <101602917+kebohning@users.noreply.github.com>
2025-01-16 14:21:44 -0800

    Add files via upload

---
+++ Week2.py
@@ -0,0 +1,28 @@
+#week 2 assignment
+#Kate Bohning
+
+#Part 1
+number = 31
+print(f"This number equals {number} in Decimal.")
+print(f"this number equals {bin(number)} in Binary.")
+print(f"this number equals {hex(number)} in Hexadecimal.")
+
+#Part 2
+print("Part 2 will produce a Data Type Error if x = 1.825. The bin() and hex() functions")
+print("require an integer, and 1.825 is a float.")
+
+x = 18
+print(f"This number equals {x} in Decimal.")
+print(f"this number equals {bin(x)} in Binary.")
+print(f"this number equals {hex(x)} in Hexadecimal.")
+
+#Part 3
+y = 0b1011
+z = 0xA3
+
+print(f"{y}, {z}")
+
+#Part 4
+w = x + y + z
+
+print(f"The sum is {w}.")
